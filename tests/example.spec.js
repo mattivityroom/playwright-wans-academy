83e8b3d79ef41de0d89a7187f2d82f712ef164b0
mattivityroom <mattivityroom@gmail.com>
2025-03-08 15:45:08 +0700

    First Chapter

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,24 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('success login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+
+  await page.click('#login-button');
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+ });
+
+test.skip('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test.skip('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
