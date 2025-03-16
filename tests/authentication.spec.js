6a6ffe04e157d90bc3de6048cd1d4c3d8d30d278
mattivityroom <mattivityroom@gmail.com>
2025-03-16 14:42:25 +0700

    [week-2] refactor loginpage scope

--- tests/authentication.spec.js
+++ tests/authentication.spec.js
@@ -1,26 +1,20 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
-import { todo } from 'node:test';
 
 test.describe('authentication', () => {
+  let loginPage;
+
   test.beforeEach(async ({ page }) => {
+    loginPage = LoginPage(page);
     await page.goto('/');
   });
 
-  // TODO: refactor loginPage to use be
-test.beforeAll(async ({ browser }) => {
-  let context = await browser.newContext();
-  let page = await context.newPage();
-});
-
   test('success login', async ({ page }) => {
-    const loginPage = LoginPage(page);
     await loginPage.login('standard_user', 'secret_sauce');
   });
 
   test('user logout', async ({ page }) => {
-    const loginPage = LoginPage(page);
     await loginPage.login('standard_user', 'secret_sauce');
 
     await page.click('#react-burger-menu-btn')
@@ -30,13 +24,11 @@ test.beforeAll(async ({ browser }) => {
   });
 
   test('user locked out', async ({ page }) => {
-    const loginPage = LoginPage(page);
     await loginPage.login('locked_out_user', 'secret_sauce');
     await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Sorry, this user has been locked out.');
   });
 
   test('invalid authentication', async ({ page }) => {
-    const loginPage = LoginPage(page);
     await loginPage.login('invalid_user', 'invalid_password');
     await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service');
   });
